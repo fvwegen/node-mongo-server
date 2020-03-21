34ac5109d054c82293f046827fb41c7947d84407
Frans van Wegen <frans@fmg.nl>
2020-03-21 13:50:51 +0100

    first commit

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb://10.2.111.66:27017/cinema', { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
